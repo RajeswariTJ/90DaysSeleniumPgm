cbe538beaf13f2731194e7b82ceb4e1d45cded15
RajeswariTJ <66548194+RajeswariTJ@users.noreply.github.com>
2020-08-04 21:49:17 +0530

    Day2 java program

---
+++ javaPgm/day2/RandamNumber.java
@@ -0,0 +1,11 @@
+package javaPgm.day2;
+
+public class RandamNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Random number: " +Math.round(Math.random()*100000000));
+		
+	}
+
+}
